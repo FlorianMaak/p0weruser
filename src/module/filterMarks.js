91c315403fffff9014d2eec8c389a5014dae8679
Florian Maak <fmaak@outlook.de>
2017-11-20 21:37:05 +0100

    fix(WidescreenMode): Restored loader

---
+++ src/module/filterMarks.js
@@ -0,0 +1,9 @@
+export default class FilterMarks {
+    constructor() {
+        this.name = 'Filtermarkierung';
+        this.description = 'Markiert Medien entsprechend ihres Filters.'
+    }
+
+    load() {
+    }
+}
