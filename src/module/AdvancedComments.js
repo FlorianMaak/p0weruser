2351b91a76e33a277afa34f2cce08aa17c9bd4c4
Florian Maak <fmaak@outlook.de>
2018-04-09 20:40:29 +0200

    feat(AdvancedComments): Added color setting

--- src/module/AdvancedComments.js
+++ src/module/AdvancedComments.js
@@ -1,9 +1,11 @@
+import Settings from '../Settings';
 import Utils from '../Utils';
 
 export default class AdvancedComments {
     constructor() {
         this.name = 'Erweiterte Kommentare';
         this.description = 'Erweitert die Kommentare um Farben und weitere Funktionen';
+        this.displayColors = Settings.get('AdvancedComments.settings.display_colors');
     }
 
 
@@ -20,8 +22,23 @@ export default class AdvancedComments {
     }
 
 
+    getSettings() {
+        return [
+            {
+                id: 'display_colors',
+                title: 'Kommentarfarben',
+                description: 'Färbe Kommentarebenen ein!'
+            }
+        ];
+    }
+
+
     prepareComments() {
         window.addEventListener('commentsLoaded', () => {
+            if (!this.displayColors) {
+                $('.comments').addClass('no-colors');
+            }
+
             const comments = $('.comments .comment-box .comment');
             comments.tooltip();
             for (let i = 0; i < comments.length; i++) {
