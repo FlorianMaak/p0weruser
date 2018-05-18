0911a0bc3ec74fabeeb7b3bfca358f51eb3537c5
Florian Maak <fmaak@outlook.de>
2018-05-18 21:16:33 +0200

    feat(ViewedPostsMarker): Mark viewed posts

---
+++ src/module/ViewedPostsMarker.js
@@ -0,0 +1,56 @@
+import Settings from '../Settings';
+import Utils from '../Utils';
+
+export default class ViewedPostsMarker {
+    constructor() {
+        this.id = 'ViewedPostsMarker';
+        this.name = '"Bereits gesehen"-Markierung';
+        this.description = 'Markiert bereits gesehene Medien.';
+    }
+
+    load() {
+        let _this = this;
+        this.styles = require('../style/viewedPostsMarker.less');
+        this.viewedPosts = ViewedPostsMarker.getViewedPosts();
+
+        $(document).ajaxComplete((event, request, settings) => {
+            for (let i = 0; i < this.viewedPosts.length; i++) {
+                ViewedPostsMarker.markAsViewed(this.viewedPosts[i]);
+            }
+        });
+
+        p.View.Stream.Item = p.View.Stream.Item.extend({
+            show: function (rowIndex, itemData, defaultHeight, jumpToComment) {
+                this.parent(rowIndex, itemData, defaultHeight, jumpToComment);
+
+                _this.addViewedPost(itemData.id);
+            }
+        });
+
+        // Fix audio-controls
+        Utils.addVideoConstants();
+    }
+
+    static markAsViewed(id) {
+        let elem = document.getElementById('item-' + id);
+
+        if (elem) {
+            elem.classList.add('viewed');
+        }
+    }
+
+    static getViewedPosts() {
+        let posts = Settings.get('viewed_posts');
+
+        if (posts === true) {
+            return [];
+        }
+
+        return JSON.parse(posts);
+    }
+
+    addViewedPost(id) {
+        this.viewedPosts.push(id);
+        Settings.set('viewed_posts', JSON.stringify(this.viewedPosts));
+    }
+}
