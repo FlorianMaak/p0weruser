fbf05846d9b7424a67810e507b40d00c690c8420
Florian Maak <fmaak@outlook.de>
2018-11-27 23:12:54 +0100

    feat(WidescreenMode): Added comment-styles

--- src/module/WidescreenMode.js
+++ src/module/WidescreenMode.js
@@ -251,6 +251,29 @@ export default class WidescreenMode {
 
             return `<div class="item-row">${result}</div>`;
         };
+
+        // Handle messages
+        p.View.InboxMessages.Messages = p.View.InboxMessages.Messages.extend({
+            show: function () {
+                this.data.messages.reverse();
+                this.parent();
+
+                let pane = $('.pane');
+                let top = pane.offset().top;
+                let iPane = pane.find('.inbox-messages');
+                let cPane = pane.find('.conversations-pane');
+
+                // Handle height
+                pane.addClass('private-message-pane');
+                pane.css('height', `calc(100vh - ${top}px)`);
+
+                // Handle scrollbars
+                let iScroll = new SimpleBar(iPane[0]);
+                let cScroll = new SimpleBar(cPane[0]);
+
+                $(iScroll.getScrollElement()).scrollTop(iScroll.getScrollElement().scrollHeight)
+            }
+        });
     }
 
 
