4e9d9acccaa53978a74bd98fcf552caf88f2ceb6
Florian Maak <fmaak@outlook.de>
2018-11-28 20:59:19 +0100

    fix(Chat): Removed flickering

--- src/module/Chat.js
+++ src/module/Chat.js
@@ -14,25 +14,29 @@ export default class Chat {
     }
 
     overrideView() {
+        p.View.InboxMessages.Conversations = p.View.InboxMessages.Conversations.extend({
+            show: function () {
+                let pane = this.$container.parents('.pane');
+                let top = pane.offset().top;
+                pane.addClass('private-message-pane');
+                pane.css('height', `calc(100vh - ${top}px)`);
+
+                let cPane = pane.find('.conversations-pane');
+                let cScroll = new SimpleBar(cPane[0]);
+
+                this.parent();
+            }
+        });
+
         p.View.InboxMessages.Messages = p.View.InboxMessages.Messages.extend({
             show: function () {
                 this.data.messages.reverse();
                 this.parent();
 
-                let pane = $('.pane');
-                let top = pane.offset().top;
-                let iPane = pane.find('.inbox-messages');
-                let cPane = pane.find('.conversations-pane');
-
-                // Handle height
-                pane.addClass('private-message-pane');
-                pane.css('height', `calc(100vh - ${top}px)`);
-
-                // Handle scrollbars
+                let iPane = this.$container.parents('.inbox-messages');
                 let iScroll = new SimpleBar(iPane[0]);
-                let cScroll = new SimpleBar(cPane[0]);
 
-                $(iScroll.getScrollElement()).scrollTop(iScroll.getScrollElement().scrollHeight)
+                $(iScroll.getScrollElement()).scrollTop(iScroll.getScrollElement().scrollHeight);
             }
         });
     }
